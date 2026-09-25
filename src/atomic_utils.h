e95825fa07e5952037ecfdfd54404db8c9085752
16aryank <arykumar@umich.edu>
2026-09-25 14:54:50 -0400

    fix: made atomic concept trivial

--- src/atomic_utils.h
+++ src/atomic_utils.h
@@ -7,8 +7,8 @@ template <class T>
 concept LockFreeAtomicValue =
     std::is_trivially_copyable_v<T> &&
     std::is_same_v<T, std::remove_cv_t<T>> &&
-    std::is_copy_constructible_v<T> &&
-    std::is_move_constructible_v<T> &&
-    std::is_copy_assignable_v<T> &&
-    std::is_move_assignable_v<T> &&
+    std::is_trivially_copy_constructible_v<T> &&
+    std::is_trivially_move_constructible_v<T> &&
+    std::is_trivially_copy_assignable_v<T> &&
+    std::is_trivially_move_assignable_v<T> &&
     std::atomic<T>::is_always_lock_free;
